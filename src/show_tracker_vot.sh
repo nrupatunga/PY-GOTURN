0fafe4727309aaa1028bd23f84b1406afd4cf597
nrupatunga <nrupatunga.tunga@gmail.com>
2017-06-12 12:23:54 +0530

    Adding new folder heirarchy for goturn

--- src/show_tracker_vot.sh
+++
@@ -1,1 +0,0 @@
-python ./show_tracker_vot.py --p '../../nets/tracker.prototxt' --m '../../nets/models/pretrained_model/tracker.caffemodel' --i '/media/nrupatunga/data/datasets/VOT/VOT2014/vot2014/' --g 0
